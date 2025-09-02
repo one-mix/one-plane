cacc5bedcf3b602f99b0f8b4573ed1d282a3da2e
dongcarry96 <fhakwpak6@naver.com>
2025-09-03 00:40:50 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/oneplane/OneplaneApplication.java
@@ -0,0 +1,13 @@
+package com.oneplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneplaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneplane/OneplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
