4a7fac958f2514c85ab87e69f62c78f55281cada
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-09-09 10:39:59 +0900

    fix: merge conflict #15

--- src/main/java/com/oneplane/alert/service/AlertLevelServiceImpl.java
+++ src/main/java/com/oneplane/alert/service/AlertLevelServiceImpl.java
@@ -1,37 +1,5 @@
 package com.oneplane.alert.service;
 
-<<<<<<< HEAD
-import com.oneplane.alert.dao.AlertLevelDao;
-import com.oneplane.alert.domain.AlertLevel;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class AlertLevelServiceImpl implements AlertLevelService {
-    private final AlertLevelDao alertLevelDao;
-
-    public AlertLevelServiceImpl(AlertLevelDao alertLevelDao) {
-        this.alertLevelDao = alertLevelDao;
-    }
-
-    @Override
-    public AlertLevel getAlertByCountryId(Long countryId) {
-        return alertLevelDao.selectAlertByCountryId(countryId);
-    }
-
-    @Override
-    public void saveAlert(AlertLevel alertLevel) {
-        alertLevelDao.insertAlert(alertLevel);
-    }
-
-    @Override
-    public List<AlertLevel> getAllAlerts() {
-        return alertLevelDao.selectAllAlerts();
-    }
-}
-=======
-
 import com.oneplane.alert.dto.AlertLevelDTO;
 import com.oneplane.alert.dto.TravelWarningApiResponse;
 import com.oneplane.alert.repository.AlertLevelRepository;
@@ -160,4 +128,3 @@ public class AlertLevelServiceImpl implements AlertLevelService {
         }
     }
 }
->>>>>>> dev
