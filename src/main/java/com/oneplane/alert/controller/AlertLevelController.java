bae6be6c29613179b8cc11ee5377703ce7316157
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-09-09 10:44:09 +0900

    fix: merge conflict #15

--- src/main/java/com/oneplane/alert/controller/AlertLevelController.java
+++ src/main/java/com/oneplane/alert/controller/AlertLevelController.java
@@ -12,23 +12,8 @@ public class AlertLevelController {
     private final AlertLevelService alertLevelService;
 
     public AlertLevelController(AlertLevelService alertLevelService) {
+
         this.alertLevelService = alertLevelService;
     }
 
-    @GetMapping("/{countryId}")
-    public AlertLevel getAlert(@PathVariable Long countryId) {
-        return alertLevelService.getAlertByCountryId(countryId);
-    }
-
-    @GetMapping("/all")
-    public List<AlertLevel> getAllAlerts() {
-        return alertLevelService.getAllAlerts();
-    }
-
-
-    @PostMapping("/add")
-    public String addAlert(@RequestBody AlertLevel alertLevel) {
-        alertLevelService.saveAlert(alertLevel);
-        return "등록 완료";
-    }
 }
