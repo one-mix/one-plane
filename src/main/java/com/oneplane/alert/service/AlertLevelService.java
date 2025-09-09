961dab54a0348abf2aed818b7382a2b6a5c3790f
Oh, Sukyeong <osukyeong0109@gmail.com>
2025-09-09 10:34:26 +0900

    fix: merge conflict #15

--- src/main/java/com/oneplane/alert/service/AlertLevelService.java
+++ src/main/java/com/oneplane/alert/service/AlertLevelService.java
@@ -1,19 +1,8 @@
 package com.oneplane.alert.service;
 
-<<<<<<< HEAD
-import com.oneplane.alert.domain.AlertLevel;
-import java.util.List;
-
-public interface AlertLevelService {
-    AlertLevel getAlertByCountryId(Long countryId);
-    void saveAlert(AlertLevel alertLevel);
-
-    List<AlertLevel> getAllAlerts();
-=======
 import com.oneplane.alert.dto.TravelWarningApiResponse;
 
 public interface AlertLevelService {
     String determineLevelValue(TravelWarningApiResponse.Item item);
     void fetchAndSaveAlertLevels();
->>>>>>> dev
 }
