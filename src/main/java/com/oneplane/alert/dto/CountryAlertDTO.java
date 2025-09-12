7de16f727df5cdd6741e0ada8d3926e8a4988d7a
daehyuk1231 <daehyuk1231@naver.com>
2025-09-12 09:32:26 +0900

    FEAT: 관리자 국가 위도 경도 추가 #45

--- src/main/java/com/oneplane/alert/dto/CountryAlertDTO.java
+++ src/main/java/com/oneplane/alert/dto/CountryAlertDTO.java
@@ -10,4 +10,7 @@ public class CountryAlertDTO {
     private String isoCode;
     private String levelValue;
     private String countryImg;
+
+    private Double countryLatitude;
+    private Double countryLongitude;
 }
